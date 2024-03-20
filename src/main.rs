d87b26edec07df752f348565bf308e7a9fe69af7
redScorpe <119797810+redScorpes@users.noreply.github.com>
2024-03-20 23:30:35 +0100

    Fixed a problem with the windows terminal where the program would close prematurely.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use rand::Rng;
 use std::io;
 use std::thread;
 use std::time::Duration;
+use std::io::{Read};
 use std::io::Write;
+use std::process::Command;
 
 fn main() {
     println!("How many sides shall your dice have?");
@@ -12,7 +14,7 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    let mut input: i32 = input.trim().parse().expect("not a number");
+    let input: i32 = input.trim().parse().expect("not a number");
 
     let random_number = rand::thread_rng().gen_range(1..=input);
 
@@ -31,4 +33,16 @@ fn main() {
     thread::sleep(Duration::from_secs(1));
 
     println!("\nYour number is {}", random_number);
+
+    println!("Press any key to exit...");
+    let _ = std::io::stdin().read(&mut [0u8]);
+
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(&["/C", "exit"])
+            .status()
+            .expect("Failed to execute command");
+    } else {
+        println!("This only works on Windows.");
+    }
 }
